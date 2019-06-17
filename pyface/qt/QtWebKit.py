eeac19339a47eb5bdb84ba740618cc4450f41f92
braidedlogix <braidedlogix@users.noreply.github.com>
2019-06-17 08:35:28 -0500

    qt html issues

--- pyface/qt/QtWebKit.py
+++ pyface/qt/QtWebKit.py
@@ -4,11 +4,14 @@ if qt_api == 'pyqt':
     from PyQt4.QtWebKit import *
 
 elif qt_api == 'pyqt5':
-    from PyQt5.QtWidgets import *
+    
     try:
         from PyQt5.QtWebKitWidgets import *
     except:
         from PyQt5.QtWebEngineWidgets import *
+        from PyQt5 import QtWidgets, QtCore
+        QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
+    from PyQt5.QtWidgets import *    
 
 else:
     from PySide.QtWebKit import *
